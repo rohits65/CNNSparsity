7aff5870dcc3a47f0b3d2c1f9ab5cde61f9a6e9d
Rohit Suresh <rs670@duke.edu>
2025-04-25 22:42:23 -0400

    updated sparsity methods

--- resnet.py
+++ resnet.py
@@ -2,8 +2,36 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Base ResNet20 class for CIFAR-10
-# General class being kept in a separate file if needed in any sparsity method
+
+
+class ResNet20(nn.Module):
+    def __init__(self, n_classes):
+        super(ResNet20, self).__init__()
+        self.in_chan = 16
+        
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bnorm1 = nn.BatchNorm2d(16)
+
+        self.residual_layers = nn.Sequential(
+            ResBlock(16, 16, stride=1), ResBlock(16, 16, stride=1), ResBlock(16, 16, stride=1),
+
+            ResBlock(16, 32, stride=2), ResBlock(32, 32, stride=1), ResBlock(32, 32, stride=1),
+
+            ResBlock(32, 64, stride=2), ResBlock(64, 64, stride=1), ResBlock(64, 64, stride=1),
+        )
+
+        self.pool = nn.AvgPool2d(8)
+        self.classifier = nn.Linear(64, n_classes)
+    
+    def forward(self, x):
+        x = torch.relu(self.bnorm1(self.conv1(x)))  
+        x = self.residual_layers(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
 
 class ResBlock(nn.Module):
     def __init__(self, in_chan, out_chan, stride):
@@ -28,32 +56,97 @@ class ResBlock(nn.Module):
         return torch.relu(x)
 
 
-class ResNet20(nn.Module):
-    def __init__(self):
-        super(ResNet20, self).__init__()
-        self.in_chan = 16
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bnorm1 = nn.BatchNorm2d(16)
 
-        self.residual_layers = nn.Sequential(
-            ResBlock(16, 16, stride=1), ResBlock(16, 16, stride=1), ResBlock(16, 16, stride=1),
+class BasicBlock(nn.Module):
+    expansion = 1
 
-            ResBlock(16, 32, stride=2), ResBlock(32, 32, stride=1), ResBlock(32, 32, stride=1),
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
 
-            ResBlock(32, 64, stride=2), ResBlock(64, 64, stride=1), ResBlock(64, 64, stride=1),
-        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
 
-        self.pool = nn.AvgPool2d(8)
-        self.classifier = nn.Linear(64, 10) # 10-digit CIFAR-10 classification
-    
     def forward(self, x):
-        x = torch.relu(self.bnorm1(self.conv1(x)))  
-        x = self.residual_layers(x)
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
 
 
 # _AFFINE = True
